98ee4f5a26aacd6e6854561376c1082e1932631a
tomBeach <tbeach2k@gmail.com>
2015-08-20 19:39:28 -0400

    Option A seems to be working still don't know how to get files to see each other

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,24 @@
 $(document).ready(function(){
 
+  // ======= Task =======
   function Task(params){
     if(params) this.data = params;
     this.render();
   }
+
   Task.prototype = {
     render: function(){
       this.view = "<li class='task'><input type='hidden' name='id' value='" + (this.data.id || "") + "' /><input type='hidden' name='listId' value='" + this.data.listId + "' /><textarea name='content'>" + (this.data.content || "") + "</textarea>";
       if(!this.data.id){
         this.view += "<button type='button' class='create'>Create</button>";
-      }else{
+      } else {
         this.view += "<button type='button' class='save'>Save</button><button type='button' class='delete'>Delete</button>";
       }
       this.view += "</li>";
     }
   }
 
+  // ======= List =======
   function List(params, tasksHtml){
     if(params){
       this.data = params;
@@ -23,6 +26,7 @@ $(document).ready(function(){
       this.render();
     }
   }
+
   List.prototype = {
     render: function(){
       this.view = "<div class='list'><input type='hidden' name='id' value='" + (this.data.id || "") + "' /><input type='text' name='title' value='" + (this.data.title || "") + "' />";
@@ -36,13 +40,14 @@ $(document).ready(function(){
     }
   }
 
+  // ======= views =======
   var lists;
   var tasks;
   $.ajax({
     url: "/lists",
     method: "get"
   }).then(function(response){
-    lists = response;
+    lists = response;             // list data
     lists.push({title: ""});
     return $.ajax({
       url: "/tasks",
@@ -51,98 +56,87 @@ $(document).ready(function(){
   }).then(function(response){
     var tasksHtml, task, list;
     var listsHtml = "";
-    tasks = response;
-    for(var l = 0; l < lists.length; l++){
+    tasks = response;             // task data
+
+    for(var l = 0; l < lists.length; l++){    // loop through lists
       list = lists[l];
       tasksHtml = "";
       tasks.push({listId: list.id, content: ""});
-      for(var t = 0; t < tasks.length; t++){
+
+      for(var t = 0; t < tasks.length; t++){    // loop through tasks
         if(list.id == 0) break;
         task = tasks[t];
         if(task.listId != list.id) continue;
-        tasksHtml += new Task(task).view;
+        tasksHtml += new Task(task).view;       // get task view html
       }
-      listsHtml += new List(list, tasksHtml).view;
+      listsHtml += new List(list, tasksHtml).view;   // merge list/task view html
     }
-    $("main").html(listsHtml);
+    $("main").html(listsHtml);        // set main html to listsHtml
 
-    $(".list>.create").on("click", function(){
-      var params = {title: $(this).siblings("[name=title]").val()}
+
+    // ======= sendAjax =======
+    function sendAjax(whichButton, whichMethod, whichUrl, whichParams) {
+      console.log("sendAjax");
+      var params;
+      if (whichParams == 0) {
+        params = {title: $(whichButton).siblings("[name=title]").val()}
+      } else {
+        params = whichParams;
+      }
+      if (!params) {params = {}};
+      var id = 0;
       $.ajax({
-        method: "post",
+        method: whichMethod,
         contentType: "application/json",
-        url: "/lists",
+        url: whichUrl,
         data: JSON.stringify(params)
       }).always(function(){
         location.reload();
       });
+    };
+
+
+    // ======= buttons =======
+    $(".list>.create").on("click", function(){                      // what is this syntax ???  ".list>.create"
+      console.log("list.create");
+      sendAjax($(".list>.create"), "post", "/lists", 0)
     });
+
     $(".list>.save").on("click", function(){
-      var params = {title: $(this).siblings("[name=title]").val()};
+      console.log("list.save");
       var id = $(this).siblings("[name=id]").val();
-      $.ajax({
-        method: "put",
-        contentType: "application/json",
-        url: "/lists/" + id,
-        data: JSON.stringify(params)
-      }).always(function(){
-        location.reload();
-      });
+      sendAjax($(".list>.save"), "put", ("/lists/" + id), 0);
     });
+
     $(".list>.delete").on("click", function(){
+      console.log("list.delete");
       var id = $(this).siblings("[name=id]").val();
-      $.ajax({
-        method: "delete",
-        contentType: "application/json",
-        url: "/lists/" + id
-      }).always(function(response){
-        console.dir(response)
-        location.reload();
-      });
+      sendAjax($(".list>.delete"), "delete", ("/lists/" + id), 0);
     });
 
     $(".task>.create").on("click", function(){
+      console.log("task.create");
       var id = $(this).siblings("[name=id]").val();
       var listId = $(this).siblings("[name=listId]").val();
       var params = {
         content: $(this).siblings("[name=content]").val()
       };
-      $.ajax({
-        method: "post",
-        contentType: "application/json",
-        url: "/lists/" + listId + "/tasks",
-        data: JSON.stringify(params)
-      }).always(function(){
-        location.reload();
-      });
+      sendAjax($(".task>.create"), "post", ("/lists/" + listId + "/tasks"), params);
     });
+
     $(".task>.save").on("click", function(){
+      console.log("task.save");
       var id = $(this).siblings("[name=id]").val();
-      var listId = $(this).siblings("[name=listId]").val();
       var params = {
         content: $(this).siblings("[name=content]").val()
       };
-      $.ajax({
-        method: "put",
-        contentType: "application/json",
-        url: "/tasks/" + id,
-        data: JSON.stringify(params)
-      }).always(function(){
-        location.reload();
-      });
+      sendAjax($(".task>.save"), "put", ("/tasks/" + id), params);
     });
+
     $(".task>.delete").on("click", function(){
+      console.log("task.delete");
       var id = $(this).siblings("[name=id]").val();
-      var listId = $(this).siblings("[name=listId]").val();
-      $.ajax({
-        method: "delete",
-        contentType: "application/json",
-        url: "/tasks/" + id
-      }).always(function(){
-        location.reload();
-      });
+      sendAjax($(".task>.delete"), "delete", ("/tasks/" + id), 0);
     });
-
   });
-
 });
