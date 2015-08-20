191f298b3278114b21e72bc761da560dd798d256
Nicholas Olds <nolds9@yahoo.com>
2015-08-20 18:24:29 -0400

    setup MV structure and starter code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-
-
+  List.fetch().then(function(lists){
+    lists.forEach(function(list){
+      var view = new ListView(list)
+      view.render();
+    })
+  })
 });
