9f046000721ec94fba2e17cd3bf49d2b4caa9c36
Edward Park <parkes.edward@gmail.com>
2015-08-20 20:26:22 -0400

    finished view piece

--- app/assets/javascripts/views/taskView.js
+++ app/assets/javascripts/views/taskView.js
@@ -1,4 +1,18 @@
 var TaskView = function(task){
-  this.task = task;//possible issue here.
-  this.$el=$("<div class ='task'></div>");
+  this.task = task;
+  this.$el=$("<div class ='task'></div>");//this CREATES div placeholder
+  this.render();
+  $(".tasks").append(this.$el);
 };
+
+TaskView.prototype ={
+  render: function(){
+    var self = this;
+    self.$el.html(self.taskTemplate(self.task));//replacing <div class = 'task'> with content here </div>
+  },//end of render
+  taskTemplate: function(task){
+    var html = $("<div>");//targets<div>
+    html.append("<h3>this is the task Template"+ task.id +"</h3>");
+    return(html);
+  }
+};//end of Taskview.prototype
