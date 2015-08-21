1bd147a5173a3fd628635d7a51e3cae4b2c4c2c3
Robert <i.am.robert.benjamin@gmail.com>
2015-08-20 23:24:30 -0400

    Display all lists on index.html using AJAX
    Hell yea, I’m super kickass and amazing! I’m the best programmer in the
    world! I literally made an AI with CSS! Yes! Yes! I did multivariable
    calculus in binary with my eyes closed!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	$.getJSON("/lists")
+	.then(function(response) {
+			for(var i = 0; i < response.length; i++) {
+				$("main").append("<h2>" + response[i].title + "<h2>")
+			}
+		})
+});
 
+/* --------------------------------------- */
+/*
+$(document).ready(function(){
+  Artist.fetch().then(function(artists){
+    artists.forEach(function(artist){
+      var view = new ArtistView(artist)
+      view.render();
+    })
+  })
 
 });
+*/
