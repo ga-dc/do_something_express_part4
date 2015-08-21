6dd794f8e157282d8c0ea4063eaa2aafffb62b29
Silvia Brown <siltastic@gmail.com>
2015-08-21 00:50:37 -0400

    Added export modules and basic ajax.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+  $.ajax({
+    url: "/lists",
+    method: "get"
+  })
 
-
+  .then(function(lists){
+    for(var i = 0; i <lists.length; i++){
+      var list = lists[i]
+      // 
+      var $el = $("<div class='list'>" + list.title + "</div>")
+      
+      $("main").append($el)
+    }
+  });
 });
