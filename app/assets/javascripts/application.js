4975f23d81c83d31a457fe7d6b6aee0ceadac4a0
Erica Andersen <andersenek@gmail.com>
2015-08-20 23:21:12 -0400

    Added list view functionality, CSS list title style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
 
+  var List = function(info){
+  this.title = info.title;
+  this.id = info.id;
+  };
+
+  $.ajax({
+    url: "/lists",
+    method: "get"
+  }).then(function(response) {
+    var lists = [];
+    for(var i = 0; i < response.length; i++){
+      lists.push(new List(response[i]));
+    }
+    return lists.forEach(function(list){
+      console.log("list: " + list)
+      var html = $("main");
+      html.append("<h5>Lists: " + list.title + "</h5>");
+      return(html);
+     });
+    })
+    .fail(function(response){
+      console.log("js failed to load");
+    });
+
 
 });
