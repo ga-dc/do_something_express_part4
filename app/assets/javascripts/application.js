a714efdd4a8609888c50315f03713498949377dc
Matt Ficke <matthew.ficke@gmail.com>
2015-08-21 00:01:05 -0400

    add initial page load functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
-
+  List.fetch().then(function(lists){
+    artists.forEach(function(artist){
+      var view = new ListView(list);
+      view.render();
+    })
+  })
 
 });
