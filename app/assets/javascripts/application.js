37d1039874c3151afc76482c21de389d291728ef
Robert <i.am.robert.benjamin@gmail.com>
2015-08-20 23:30:12 -0400

    Add list model

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,21 @@ $(document).ready(function(){
   })
 
 });
+
+Artist.fetch = function(){
+  var request = $.getJSON("http://localhost:3000/artists")
+  .then(function(response) {
+    var artists = []
+    for(var i = 0; i < response.length; i++){
+      artists.push(new Artist(response[i]))
+    }
+    return artists
+    })
+  .fail(function(response){
+      console.log("js failed to load")
+    })
+  return request
+}
+
+
 */
---
+++ app/assets/javascripts/models/list.js
@@ -0,0 +1,14 @@
+var List = function(info) {
+	this.title = info.title;
+	this.id = info.id
+}
+
+
+/* EXAMPLE:
+var Artist = function(info){
+  this.name = info.name;
+  this.photoUrl = info.photoUrl;
+  this.nationality = info.nationality;
+  this.id = info.id
+}
+*/
