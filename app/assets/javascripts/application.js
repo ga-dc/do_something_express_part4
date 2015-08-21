e051d3cc0d219c07614be679e518f8e8a23249e8
Silvia Brown <siltastic@gmail.com>
2015-08-21 01:04:34 -0400

    Removed comment.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
   .then(function(lists){
     for(var i = 0; i <lists.length; i++){
       var list = lists[i]
-      // 
       var $el = $("<div class='list'>" + list.title + "</div>")
       
       $("main").append($el)
