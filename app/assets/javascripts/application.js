47dd903fa0fd72dd90c5be37f636c35d22e03d80
Cameron Fry <cameronfry11@gmail.com>
2015-08-21 09:09:40 -0400

    Added the list view and tried to work through some of the logic in application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,43 @@
 $(document).ready(function(){
-
-
+  List.prototype = {
+    render: function(){
+      this.view = "<div class='list'><input type='hidden' name='id' value='" + (this.data.id || "") + "' /><input type='text' name='title' value='" + (this.data.title || "") + "' />";
+      if(!this.data.id){
+        this.view += "<button type='button' class='create'>Create</button>";
+      }else{
+        this.view += "<button type='button' class='save'>Save</button><button type='button' class='delete'>Delete</button>";
+        this.view += "<ul>" + this.tasksHtml + "</ul>";
+      }
+      this.view += "</div>";
+    }
 });
+
+var lists;
+  var tasks;
+  $.ajax({
+    url: "/lists",
+    method: "get"
+  }).then(function(response){
+    lists = response;
+    lists.push({title: ""});
+    return $.ajax({
+      url: "/tasks",
+      method: "get"
+    })
+  }).then(function(response){
+    var tasksHtml, task, list;
+    var listsHtml = "";
+    tasks = response;
+    for(var l = 0; l < lists.length; l++){
+      list = lists[l];
+      tasksHtml = "";
+      tasks.push({listId: list.id, content: ""});
+      for(var t = 0; t < tasks.length; t++){
+        if(list.id == 0) break;
+        task = tasks[t];
+        if(task.listId != list.id) continue;
+        tasksHtml += new Task(task).view;
+      }
+      listsHtml += new List(list, tasksHtml).view;
+    }
+    $("main").html(listsHtml);
