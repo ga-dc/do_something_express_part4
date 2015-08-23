3db2ca2b6fbc6d3a2234a5140e7ff356a1d25c12
lanak1301 <musiconstellation@gmail.com>
2015-08-23 15:43:37 -0400

    added render edit form function to task

--- app/assets/javascripts/views/taskView.js
+++ app/assets/javascripts/views/taskView.js
@@ -1,65 +1,54 @@
 var TaskView = function(task) {
   this.task = task;
-
+  this.$el = $('<div class="task"></div>');
 }
 
 TaskView.prototype = {
   render: function() {
     var self = this;
-    var $el = this.taskTemplate(this.task);
+    this.$el.html(this.taskTemplate(this.task));
 
-    var completeIcon = $el.find(".fa.fa-check-square");
-    var editIcon = $el.find(".fa.fa-pencil-square");
-    var deleteIcon = $el.find(".fa.fa-trash-o");
+    var completeIcon = this.$el.find(".fa.fa-check-square");
+    var editIcon = this.$el.find(".fa.fa-pencil-square");
+    var deleteIcon = this.$el.find(".fa.fa-trash-o");
 
     completeIcon.on('click', function() {
       console.log('complete icon click')
     });
     editIcon.on('click', function() {
-      console.log('edit icon click')
       self.renderEditForm();
     });
-    deleteIcon.on('click', function() {
-      self.task.destroy().then(function() { $el.fadeOut()});
-    })
-
-    return $el;
+    
+    return this.$el;
   },
   renderEditForm: function() {
-    console.log('render edit form')
     var self = this;
-    var $el = this.taskEditTemplate(this.task);
-    console.log(this.task)
+    this.$el.html(this.taskEditTemplate(this.task));
 
-    var saveIcon = $el.find(".fa.fa-floppy-o");
-    var deleteIcon = $el.find(".fa.fa-trash-o");
+    var saveIcon = this.$el.find(".fa.fa-floppy-o");
+    var deleteIcon = this.$el.find(".fa.fa-trash-o");
 
     saveIcon.on('click', function() {
       console.log('save icon click')
     })
     deleteIcon.on('click', function() {
-      console.log('delete icon click')
       self.task.destroy().then(function() { self.$el.fadeOut()});
     })
 
-    console.log($el)
-    return $el;
+    console.log($el);
   },
   taskTemplate: function(task) {
-    var html = $("<div class='task'>");
+    var html = $("<div>");
     html.append("<div class='col-xs-8'>" + this.task.body + "</div>");
     html.append("<div class='col-xs-1'><i class='fa fa-check-square'></i></div>");
     html.append("<div class='col-xs-1'><i class='fa fa-pencil-square'></i></div>");
-    html.append("<div class='col-xs-1'><i class='fa fa-trash-o'></i></div>");
     return(html);
   },
   taskEditTemplate: function(task) {
-    console.log(task)
     var html = $("<div>");
     html.append("<div class='col-xs-8'><input name='body' value='" + this.task.body + "'></div>");
     html.append("<div class='col-xs-1'><i class='fa fa-floppy-o'></i></div>");
     html.append("<div class='col-xs-1'><i class='fa fa-trash-o'></i></div>");
-    console.log(html)
     return(html);
 
   }
